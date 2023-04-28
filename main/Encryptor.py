eb18c5be5f91be3c4258257dd060e42396c11b2f
Pixol20 <115364463+pixol20@users.noreply.github.com>
2023-04-28 09:54:39 +0300

    added encryption

--- main/Encryptor.py
+++ main/Encryptor.py
@@ -1,1 +1,43 @@
-print("Start here")
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+
+def encrypt(plaintext, password):
+
+    # generate random salt
+    salt = get_random_bytes(AES.block_size)
+    # generate key
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+    cipher_config = AES.new(private_key, AES.MODE_GCM)
+
+    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plaintext, 'utf-8'))
+    return {
+        'cipher_text': b64encode(cipher_text).decode('utf-8'),
+        'salt': b64encode(salt).decode('utf-8'),
+        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
+        'tag': b64encode(tag).decode('utf-8')
+    }
+
+
+def decrypt(encrypted, password):
+    # decode the dictionary entries from base64
+    salt = b64decode(encrypted['salt'])
+    cipher_text = b64decode(encrypted['cipher_text'])
+    nonce = b64decode(encrypted['nonce'])
+    tag = b64decode(encrypted['tag'])
+
+    # generate the private key from the password and salt
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+    # create the cipher config
+    cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+    # decrypt the cipher text
+    decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+
+    return bytes.decode(decrypted)
